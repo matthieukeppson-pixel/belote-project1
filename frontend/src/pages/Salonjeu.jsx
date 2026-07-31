91994dd573fb95d70a8798b79edf3282110ea4aa
matthieukeppson-pixel <matthieukeppson@gmail.com>
2026-07-31 21:20:19 +0000

    Add Boude chat emoji

--- frontend/src/pages/Salonjeu.jsx
+++ frontend/src/pages/Salonjeu.jsx
@@ -66,6 +66,7 @@ const SALON_EMOJIS = [
   { code: ":flute_champagne:", src: "/emojis/flute_champagne.png", alt: "flûte champagne" },
   { code: ":bonjour:", src: "/emojis/bonjour.png", alt: "bonjour" },
   { code: ":aurevoir:", src: "/emojis/aurevoir.png", alt: "au revoir" },
+  { code: ":boude:", src: "/emojis/boude.png", alt: "boude" },
 ];
 
 const SALON_CHAT_EMOJIS = Object.fromEntries(
